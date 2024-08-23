3ca5dd3edee9d50e0c311e53e034c7aa0676525a
周圣道 <zsdnishishui@163.com>
2024-08-23 17:52:35 +0800

    链表也可以递归遍历

--- array_linkedlist/linkedlist.py
+++ array_linkedlist/linkedlist.py
@@ -3,40 +3,55 @@
 """
 
 
-class Node:
+class ListNode:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-def get_size(node: Node):
+def get_size(node: ListNode):
     pass
 
 
-def get_index(node: Node):
+def get_index(node: ListNode):
     pass
 
 
-def insert(nodeBefore: Node, nodeAfter: Node):
+def insert(nodeBefore: ListNode, nodeAfter: ListNode):
     pass
 
 
-def append(node: Node):
+def append(node: ListNode):
     pass
 
 
-def delete(node: Node):
+def delete(node: ListNode):
     pass
 
 
-def get_value(node: Node, index):
+def get_value(node: ListNode, index):
     pass
+
+
+def recursion(l: ListNode):
+    if l is None:
+        return
+    print(l.value)
+    recursion(l.next)
 
 
 if __name__ == '__main__':
     # 初始化
-    print()
+    l = ListNode(1)
+    l2 = ListNode(2)
+    l3 = ListNode(4)
+    l.next = l2
+    l2.next = l3
     # 添加
     # 删除
     # 查找
     # 遍历
+    # while l:
+    #     print(l.value)
+    #     l = l.next
+    recursion(l)
